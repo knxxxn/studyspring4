25e77c8f5b188dbc106689b1437b09c716d1bdf0
knxxxn <20211308@sungshin.ac.kr>
2024-10-02 13:35:47 +0900

    스플이 데이터 jpa 분석
    jpa 구현체 분석, 새로운 엔티티 구별 방법, 나머지 기능들

--- data-jpa/data-jpa/src/main/java/study/data_jpa/repository/MemberRepository.java
+++ data-jpa/data-jpa/src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -13,7 +13,7 @@ import study.data_jpa.entity.Member;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom  {
+public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecificationExecutor<Member>  {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
     Page<Member> findByAge(int age, Pageable pageable);
 
